3e181d35d8bee48bd80a8253bb86dbb39bb9aa60
Scott Mayberry <sctmayberry@gmail.com>
2017-06-20 15:17:54 -0400

    Testing Commit

--- app/src/main/java/com/example/sctma/autosteward/MainActivity.java
+++ app/src/main/java/com/example/sctma/autosteward/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     Button trashButton;
     Button trashFineButton;
     Button kitchenFineButton;
+    Button asdfasdf;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
