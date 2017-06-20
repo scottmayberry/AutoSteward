07eae32f47edfd32ab8eab379bf544f8aa036968
Mayberry <SMAYBER8@ford.com>
2017-06-20 15:15:57 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AutoSteward
+This application is made for Chi Phi Fraternity at MIT.
+It utilizes the Firebase database set up, and acts as the client side. The server commands are handled from elsewhere.
---
+++ app/src/main/java/com/example/sctma/autosteward/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.sctma.autosteward;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Space;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button dishButton;
+    Button dishFineButton;
+    Button midnightsButton;
+    Button midnightsFineButton;
+    Button trashButton;
+    Button trashFineButton;
+    Button kitchenFineButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        updateKitchenUI(false);
+        updateDishesUI(false);
+        updateMidnightsUI(false);
+        updateTrashUI(false);
+    }
+    protected void updateKitchenUI(boolean onOff)
+    {
+        int vis;
+        if(onOff)
+            vis = View.VISIBLE;
+        else
+            vis = View.GONE;
+        ((Space)findViewById(R.id.kitchenFineSpace)).setVisibility(vis);
+        ((LinearLayout)findViewById(R.id.kitchenFineButtonLayout)).setVisibility(vis);
+        ((LinearLayout)findViewById(R.id.kitchenFineTextLayout)).setVisibility(vis);
+    }
+    protected void updateDishesUI(boolean onOff)
+    {
+        int vis;
+        if(onOff)
+            vis = View.VISIBLE;
+        else
+            vis = View.GONE;
+        ((Space)findViewById(R.id.dishFineSpace)).setVisibility(vis);
+        ((LinearLayout)findViewById(R.id.dishFineButtonLayout)).setVisibility(vis);
+        ((LinearLayout)findViewById(R.id.dishFineTextLayout)).setVisibility(vis);
+    }
+    protected void updateTrashUI(boolean onOff)
+    {
+        int vis;
+        if(onOff)
+            vis = View.VISIBLE;
+        else
+            vis = View.GONE;
+        ((Space)findViewById(R.id.trashFineSpace)).setVisibility(vis);
+        ((LinearLayout)findViewById(R.id.trashFineButtonLayout)).setVisibility(vis);
+        ((LinearLayout)findViewById(R.id.trashFineTextLayout)).setVisibility(vis);
+    }
+    protected void updateMidnightsUI(boolean onOff)
+    {
+        int vis;
+        if(onOff)
+            vis = View.VISIBLE;
+        else
+            vis = View.GONE;
+        ((Space)findViewById(R.id.midnightsFineSpace)).setVisibility(vis);
+        ((LinearLayout)findViewById(R.id.midnightsFineButtonLayout)).setVisibility(vis);
+        ((LinearLayout)findViewById(R.id.midnightsFineTextLayout)).setVisibility(vis);
+    }
+}
